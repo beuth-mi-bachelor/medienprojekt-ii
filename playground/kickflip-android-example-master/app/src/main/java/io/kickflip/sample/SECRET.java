fc29cff58bdb35e5f0f546ec3ad1a76548ad84c8
Angelina Staeck <s52741@beuth-hochschule.de>
2015-04-29 21:53:31 +0200

    kickflip kram

--- playground/kickflip-android-example-master/app/src/main/java/io/kickflip/sample/SECRET.java
+++
@@ -1,5 +0,0 @@
-package io.kickflip.sample;
-public class SECRETS {
-    public static final String CLIENT_KEY = "bSvtUMt1D;KSG7svGjj-iND8RDLc=a4Msp@3kKCs";
-    public static final String CLIENT_SECRET = "Ur!2nc6H?iwQ-wyIJMPxW9ZhOvoLlQkt7z.CxiZ9WNAM6fni30SD=CBuJ;oo!FBwKZcvDSasOXZ=jLVzGZTGhfai5NLe.c0YkNipnNeeEYTW3.DvnzMj8zD-n_!?fH5o";
-}
