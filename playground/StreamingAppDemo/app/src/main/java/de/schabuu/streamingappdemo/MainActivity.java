0323ec9122ef9c8276efaa74326cbd8bcfc85979
Felix Rainer Dietmar Maulwurf <s52403@beuth-hochschule.de>
2015-05-11 13:28:56 +0200

    Fundament build no sdk in it yet

---
+++ playground/StreamingAppDemo/app/src/main/java/de/schabuu/streamingappdemo/MainActivity.java
@@ -0,0 +1,60 @@
+package de.schabuu.streamingappdemo;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public final static String ROOM_NAME = "de.schabuu.streamingappdemo.ROOM";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                changeView(v);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void changeView(View v){
+        Intent intent = new Intent(this, MainActivity2.class);
+        EditText roomName = (EditText) findViewById(R.id.editText);
+        String name = roomName.getText().toString();
+        intent.putExtra(ROOM_NAME, name);
+        startActivity(intent);
+    }
+}
