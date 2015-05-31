53360608bd0f4c7275163ef7fd85e6f9db10ec9b
Angelina Staeck <s52741@beuth-hochschule.de>
2015-05-31 22:45:39 +0200

    activities started

---
+++ playground/NanostreamPlayerTest/app/src/main/java/net/nanocosmos/PlayerActivity/MainMenuActivity.java
@@ -0,0 +1,52 @@
+package net.nanocosmos.PlayerActivity;
+
+/**
+ * Created by angi on 31.05.15.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import net.nanocosmos.NanostreamPlayerTest.R;
+
+public class MainMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        View playAloneView= findViewById(R.id.play_alone);
+        playAloneView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this, PlayAloneActivity.class));
+            }
+        });
+
+        View playWithFriendsView= findViewById(R.id.play_friends);
+        playWithFriendsView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this, PlayWithFriendsActivity.class));
+            }
+        });
+
+        View settingsView= findViewById(R.id.settings);
+        settingsView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this, SettingsView.class));
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
---
+++ playground/NanostreamPlayerTest/app/src/main/java/net/nanocosmos/PlayerActivity/PlayAloneActivity.java
@@ -0,0 +1,28 @@
+package net.nanocosmos.PlayerActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import net.nanocosmos.NanostreamPlayerTest.R;
+
+/**
+ * Created by angi on 31.05.15.
+ */
+public class PlayAloneActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_alone);
+
+        View playAloneView= findViewById(R.id.back_button);
+        playAloneView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(PlayAloneActivity.this, MainMenuActivity.class));
+            }
+        });
+    }
+}
