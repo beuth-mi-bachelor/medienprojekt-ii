4ed8c6cb8d5fb82a284a0051c95e73f53e56caff
Sprotte <sprottemail@googlemail.com>
2015-05-31 23:56:38 +0200

    Added find/create room alert

--- playground/NanostreamPlayerTest/app/src/main/java/net/nanocosmos/PlayerActivity/PlayWithFriendsActivity.java
+++ playground/NanostreamPlayerTest/app/src/main/java/net/nanocosmos/PlayerActivity/PlayWithFriendsActivity.java
@@ -1,9 +1,13 @@
 package net.nanocosmos.PlayerActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 
 import net.nanocosmos.NanostreamPlayerTest.R;
 
@@ -12,17 +16,74 @@ import net.nanocosmos.NanostreamPlayerTest.R;
  */
 public class PlayWithFriendsActivity extends Activity {
 
+    Editable value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_with_friends);
 
-        View playAloneView= findViewById(R.id.back_button);
-        playAloneView.setOnClickListener(new View.OnClickListener() {
+        View backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(PlayWithFriendsActivity.this, MainMenuActivity.class));
             }
         });
+
+        View joinButton = findViewById(R.id.join);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                alert("Join Room","Enter Roomname",AlertType.JOIN);
+            }
+        });
+
+        View createButton = findViewById(R.id.create);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                alert("Create Room","Enter Roomname",AlertType.CREATE);
+            }
+        });
+
+
+    }
+
+    private void alert(final String title, String msg, final AlertType type){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle(title);
+        alert.setMessage(msg);
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                value = input.getText();
+
+                switch (type){
+                    case CREATE:
+                        //TO DO CREATE ROOM LOGIC
+                        startActivity(new Intent(PlayWithFriendsActivity.this, PlayAloneActivity.class));
+                        break;
+                    case JOIN:
+                        //TO DO ADD FIND ROOM LOGIC
+                        startActivity(new Intent(PlayWithFriendsActivity.this, PlayAloneActivity.class));
+                        break;
+                }
+
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
+    }
+    public enum AlertType {
+        CREATE, JOIN
     }
 }
+
