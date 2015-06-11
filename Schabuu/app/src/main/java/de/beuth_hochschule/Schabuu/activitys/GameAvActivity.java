f98ea6091821a7a3cc3bb0cfc1ec48de4541483c
Paul Sprotte (Exozet Mobile) <paul.sprotte@exozet.com>
2015-06-11 21:46:45 +0200

    blub

--- Schabuu/app/src/main/java/de/beuth_hochschule/Schabuu/activitys/GameAvActivity.java
+++ Schabuu/app/src/main/java/de/beuth_hochschule/Schabuu/activitys/GameAvActivity.java
@@ -17,7 +17,7 @@ public class GameAvActivity extends Activity {
 
     private String license_stream = "nlic:1.2:LiveEnc:1.1:LivePlg=1,H264ENC=1,MP4=1,RTMPsrc=1,RtmpMsg=1,RTMPm=4,RTMPx=3,Resz=1,Demo=1,Ic=1,NoMsg=1:adr:20150429,20151213::0:0:nanocosmos-471231-28:ncpt:28cd49a163eaf61a48484c9e17a5d808";
     private String serverUrl = "rtmp://ws2.nanocosmos.net/live";
-    private String streamName = "PaulTest";
+    private String streamName = "foobar";
 
     private static String authUser = "";
     private static String authPass = "";
@@ -27,6 +27,8 @@ public class GameAvActivity extends Activity {
     private static final String LOG_TAG = "GameAvActivity";
 
     private ServerConnector _server;
+
+    private StreamingUtils utils = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +41,10 @@ public class GameAvActivity extends Activity {
 
         setContentView(R.layout.activity_gamescreen_av);
 
-        if (intent.getStringExtra("MODE").equals("CAM")){
-            StreamingUtils utils = new StreamingUtils(serverUrl, streamName, license_stream, authUser, authPass, (SurfacePlayerView) findViewById(R.id.view), getApplicationContext());
+
+            utils = new StreamingUtils(serverUrl, streamName, license_stream, authUser, authPass, (SurfacePlayerView) findViewById(R.id.view), getApplicationContext());
             utils.toggleStreaming();
-        } else {
-            RecievingUtils utils = new RecievingUtils(this, license, serverUrl, streamName, authUser, authPass);
-            utils.StartPlayer();
-        }
+
 
     }
 
---
+++ playground/NanostreamPlayerTest1/app/src/main/java/net/nanocosmos/PlayerActivity/PlayerActivity.java
@@ -0,0 +1,204 @@
+package net.nanocosmos.PlayerActivity;
+
+import net.nanocosmos.nanoStream.streamer.NanostreamEvent;
+import net.nanocosmos.nanoStream.streamer.NanostreamPlayer;
+import net.nanocosmos.nanoStream.streamer.NanostreamPlayer.PlayerEventListener;
+import net.nanocosmos.nanoStream.streamer.NanostreamPlayer.PlayerSettings;
+import net.nanocosmos.nanoStream.streamer.NanostreamPlayerImpl;
+import net.nanocosmos.nanoStream.streamer.nanoStream;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+
+public class PlayerActivity extends Activity implements PlayerEventListener
+{
+
+	private RetainedFragment dataFragment;
+
+	// private RelativeLayout root;
+	private LinearLayout root;
+
+	private MediaController controller;
+
+    private static final String license = "nlic:1.2:LiveEnc:3.0:LvApp=1,LivePlg=1,H264DEC=1,H264ENC=1,RTMPsrc=1,RtmpMsg=1,RTMPx=3,NoMsg=1,Ic=0:adr,ios:20150409,20150707::0:0:smartfrog-431775-1:ncpt:90ddf42ffe204d9e1e6ac99e9df92aba";
+
+
+    private static final String strStreamUrl = "rtmp://ws2.nanocosmos.net/vod";
+	private static final String strStreamname = "mp4:sync.mp4";
+
+	private static final String authUser = "";
+	private static final String authPass = "";
+
+	private static final String LOG_TAG = "PlayerActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT, 1.0F);
+
+		root = new LinearLayout(this);
+		root.setOrientation(LinearLayout.VERTICAL);
+		root.setLayoutParams(containerParams);
+		root.setBackgroundColor(Color.BLACK);
+
+		// find the retained fragment on activity restarts
+		FragmentManager fm = getFragmentManager();
+		dataFragment = (RetainedFragment) fm.findFragmentByTag("data");
+
+		NanostreamPlayer player = null;
+
+		// create the fragment and data the first time
+		if (dataFragment == null)
+		{
+			// add the fragment
+			dataFragment = new RetainedFragment();
+			fm.beginTransaction().add(dataFragment, "data").commit();
+
+			player = nanoStream.createNanostreamPlayer();
+
+			PlayerSettings settings = player.new PlayerSettings();
+
+			settings.setLicense(license);
+			settings.setUrl(strStreamUrl);
+			settings.setStreamname(strStreamname);
+			settings.setAuthUsername(authUser);
+			settings.setAuthPassword(authPass);
+			settings.setBufferTimeMs(2000);
+
+			player.setSettings(settings);
+			player.setPlayerEventListener(this);
+
+			dataFragment.setData(player);
+		} else
+		{
+			player = dataFragment.getData();
+		}
+
+		LinearLayout.LayoutParams surfaceParams1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT, 0.5F);
+		surfaceParams1.gravity = Gravity.CENTER;
+		surfaceParams1.weight = 0.5f;
+
+		SurfacePlayerView surfaceView1 = new SurfacePlayerView(this);
+		surfaceView1.setLayoutParams(surfaceParams1);
+		surfaceView1.getHolder().addCallback(player);
+
+		root.addView(surfaceView1);
+
+		controller = new MediaController(this, false);
+		controller.setAnchorView(root);
+		controller.setMediaPlayer(player);
+
+		setContentView(root);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		controller.show(5000);
+		return super.onTouchEvent(event);
+	}
+
+	protected void onDestroy()
+	{
+		super.onDestroy();
+	}
+
+	private class SurfacePlayerView extends SurfaceView
+	{
+		SurfacePlayerView(Context context)
+		{
+			super(context);
+		}
+
+		@Override
+		protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+		{
+			int viewWidth = MeasureSpec.getSize(widthMeasureSpec);
+			int viewHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+			// TODO: Get aspect ratio from stream
+			int videoWidth = 16;
+			int videoHeight = 9;
+			double aspectScreen = (double) viewWidth / (double) viewHeight;
+			double aspectVideo = (double) videoWidth / (double) videoHeight;
+
+			if (aspectScreen > aspectVideo)
+			{
+				viewWidth = (viewHeight * videoWidth) / videoHeight;
+			} else
+			{
+				viewHeight = (viewWidth * videoHeight) / videoWidth;
+			}
+
+			setMeasuredDimension(viewWidth, viewHeight);
+		}
+	}
+
+	public class RetainedFragment extends Fragment
+	{
+
+		// data object we want to retain
+		private NanostreamPlayer mPlayer = null;
+
+		// this method is only called once for this fragment
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			// retain this fragment
+			setRetainInstance(true);
+		}
+
+		public void setData(NanostreamPlayer player)
+		{
+			this.mPlayer = player;
+		}
+
+		public NanostreamPlayer getData()
+		{
+			return mPlayer;
+		}
+	}
+
+	@Override
+	public void onPlayerEvent(NanostreamEvent event, NanostreamPlayer instance)
+	{
+		if (event.GetType() == NanostreamEvent.TYPE_RTMP_STATUS && event.GetCode() == NanostreamEvent.CODE_STREAM_STARTED)
+		{
+			Log.d("TEST", "EVENT");
+			if (instance instanceof NanostreamPlayerImpl)
+			{
+				this.runOnUiThread(new Runnable()
+				{
+
+					@Override
+					public void run()
+					{
+						// TODO Auto-generated method stub
+						root.requestLayout();
+						Log.d("TEST", "LAYOUT REQUESTED");
+
+					}
+				});
+			}
+		}
+
+	}
+}
