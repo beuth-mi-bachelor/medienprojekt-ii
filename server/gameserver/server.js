f4cace913921aef534f7d887f9fefe931e48426e
Duve, Michael <michael.duve@bild.de>
2015-06-12 15:38:48 +0200

    QR-Code download working

--- server/gameserver/server.js
+++ server/gameserver/server.js
@@ -58,9 +58,9 @@ app.get('/video', function (req, res) {
 
 app.get("/download", function (req, res) {
     res.setHeader('Content-disposition', 'attachment; filename=schabuu.apk');
-    res.sendFile(__dirname + "/apk/schabuu.apk", function(err, result) {
+    res.end(__dirname + "/apk/schabuu.apk", function(err, result) {
          if (err) {
-            res.send("not found");
+            res.send("Download gestartet");
          }
      });
 });
